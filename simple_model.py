55cf1e0c7b69c1ac594dad1c346cfd66103e7d90
Dipyaman Pramanik <77561868+dpramanik92@users.noreply.github.com>
2025-07-03 03:01:03 +0530

    Update simple_model.py

--- simple_model.py
+++ simple_model.py
@@ -1,4 +1,14 @@
 class simple_model:
+    # import libraries
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import yfinance as yf
+    import numpy as np
+    import mplfinance as mpf
+    import ta
+    import datetime
+
+    
     def __init__(self,avg_pe=18,L=1,k=1):
         print("Initialize...")
         self.avg_pe = avg_pe
