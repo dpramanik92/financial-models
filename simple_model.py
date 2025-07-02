61e8d856abd0c217b49415264b1173b8bebbd7eb
Dipyaman Pramanik <77561868+dpramanik92@users.noreply.github.com>
2025-07-03 02:59:47 +0530

    Create simple_model.py

---
+++ simple_model.py
@@ -0,0 +1,13 @@
+class simple_model:
+    def __init__(self,avg_pe=18,L=1,k=1):
+        print("Initialize...")
+        self.avg_pe = avg_pe
+        self.L = L
+        self.k =k
+
+    def get_value_signal(self,pe):
+        return 1-2*self.L/(1+np.exp(-self.k*(pe-self.avg_pe)))
+
+
+        
+    
